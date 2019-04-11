9b0deeabd8082f8a22f8ca8894e4ac8aca837778
xayctoff <petro310897@gmail.com>
2019-04-11 17:07:41 +0700

    Added CucuRunner class for project execution

---
+++ src/main/java/CucuRunner.java
@@ -0,0 +1,11 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.*;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {}
+)
+
+public class CucuRunner {
+
+}
