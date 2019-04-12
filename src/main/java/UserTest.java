ea8d08d89e92e5d7cf2506bfe9c21e12319c74f4
xayctoff <petro310897@gmail.com>
2019-04-12 14:24:27 +0700

    Added User constructor to Given

--- src/main/java/UserTest.java
+++ src/main/java/UserTest.java
@@ -3,8 +3,12 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class UserTest {
+
+    private User user;
+
     @Given("^I have a user$")
     public void iHaveAUser() {
+        this.user = new User();
     }
 
     @When("^I try to get user's login$")
