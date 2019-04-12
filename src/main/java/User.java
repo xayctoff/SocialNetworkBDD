f4188dab2ab0db7d817a707e390c1338a43ae5d3
xayctoff <petro310897@gmail.com>
2019-04-12 14:35:59 +0700

    Added getPassword method in User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -16,4 +16,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getPassword() {
+        return password;
+    }
 }
