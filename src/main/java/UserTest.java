a0ad4621d244310f529eb3b97e341ac19161ac2f
xayctoff <petro310897@gmail.com>
2019-04-12 14:23:30 +0700

    Added steps definition for get user's login scenario

---
+++ src/main/java/UserTest.java
@@ -0,0 +1,17 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class UserTest {
+    @Given("^I have a user$")
+    public void iHaveAUser() {
+    }
+
+    @When("^I try to get user's login$")
+    public void iTryToGetUserSLogin() {
+    }
+
+    @Then("^I should get user's login$")
+    public void iShouldGetUserSLogin() {
+    }
+}
