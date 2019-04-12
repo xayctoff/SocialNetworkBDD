d505167a62309d3ce3ea9335aa5bdd1e82d27cf4
xayctoff <petro310897@gmail.com>
2019-04-12 08:19:37 +0700

    Added steps definition for user's authorization scenario

---
+++ src/main/java/LoginTest.java
@@ -0,0 +1,28 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginTest {
+    @Given("^I have an authorization window$")
+    public void iHaveAnAuthorizationWindow() {
+    }
+
+
+    @When("^I entered the user login$")
+    public void iEnteredTheUserLogin() {
+    }
+
+    @And("^I entered the user password$")
+    public void iEnteredTheUserPassword() {
+    }
+
+    @And("^I pressed sign in button$")
+    public void iPressedSignInButton() {
+
+    }
+
+    @Then("^I have to enter my page$")
+    public void iHaveToEnterMyPage() {
+    }
+}
