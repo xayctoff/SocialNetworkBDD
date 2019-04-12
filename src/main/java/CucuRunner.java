fcaf1f1db9dbd1639124bbacc2e4b386151983b0
xayctoff <petro310897@gmail.com>
2019-04-12 13:07:04 +0700

    CucuRunner refactoring

--- src/main/java/CucuRunner.java
+++ src/main/java/CucuRunner.java
@@ -3,7 +3,7 @@ import cucumber.api.junit.*;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {}
+        features = {"src/templates/features"}
 )
 
 public class CucuRunner {
