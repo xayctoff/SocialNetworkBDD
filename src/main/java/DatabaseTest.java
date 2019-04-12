aad0d8999d697db65301a62db41e36bdb67cc2e5
xayctoff <petro310897@gmail.com>
2019-04-12 08:50:17 +0700

    Added steps definition for connect to the database scenario

---
+++ src/main/java/DatabaseTest.java
@@ -0,0 +1,18 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DatabaseTest {
+
+    @Given("^I haven't connection to the database$")
+    public void iHavenTConnectionToTheDatabase() {
+    }
+
+    @When("^I try to connect to the database$")
+    public void iTryToConnectToTheDatabase() {
+    }
+
+    @Then("^I should connect to the database$")
+    public void iShouldConnectToTheDatabase() {
+    }
+}
