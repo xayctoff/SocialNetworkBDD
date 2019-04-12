85fc2944390b94c4a9aeb8bcf3e22332d621c3b8
xayctoff <petro310897@gmail.com>
2019-04-12 12:06:59 +0700

    Added getInstance method & private constructor

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,2 +1,42 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Database {
+
+    private static Database instance;
+    private Statement statement;
+    private Connection connection;
+
+    private Database() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/network";
+        String username = "postgres";
+        String password = "111";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        }
+
+        catch (ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static Database getInstance() {
+
+        if (instance == null) {
+
+            try {
+                instance = new Database();
+            }
+
+            catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        return instance;
+    }
 }
