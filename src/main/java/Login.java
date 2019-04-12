004afd344d9fbb82156cef7827fa3b3d28f53387
xayctoff <petro310897@gmail.com>
2019-04-12 13:41:14 +0700

    Sign in method refactoring

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,8 +1,17 @@
+import java.sql.SQLException;
+
 public class Login {
 
     public Login() {}
 
-    public boolean signIn(String name, String password) {
+    public boolean signIn(String login, String password) {
+        Database database = Database.getInstance();
+        try {
+            return database.checkOnValidAuthorization(login, password);
+        }
 
+        catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
